02c72ca57250ccb0355f32bdf11327ddc5530e21
jooyeon <jooyeon0907@naver.com>
2025-03-04 22:42:36 +0900

    fix : REST API URI 명명 규칙에 맞게 uri 수정

--- src/main/java/com/zerobase/timate/controller/UserController.java
+++ src/main/java/com/zerobase/timate/controller/UserController.java
@@ -8,26 +8,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
 	private final UserService userService;
 
-	@GetMapping("/info")
-	public ResponseEntity<ApiResponse> info(@RequestParam Long id) {
+	@GetMapping("/{id}")
+	public ResponseEntity<ApiResponse> info(@PathVariable Long id) {
 		return ResponseEntity.ok(ApiResponse.success(userService.getUserInfo(id)));
 	}
 
-	@PostMapping("/update")
-	public ResponseEntity<ApiResponse> update(@Validated @RequestBody UserDto.Request request) {
+	@PutMapping("/{id}")
+	public ResponseEntity<ApiResponse> update(@PathVariable Long id, @Validated @RequestBody UserDto.Request request) {
 		return ResponseEntity.ok(ApiResponse.success(userService.updateUser(request)));
 	}
 
