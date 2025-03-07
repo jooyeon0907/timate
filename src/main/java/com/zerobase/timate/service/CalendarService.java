8fe5f0d2cb754376f3394220a92709383125489d
jooyeon <jooyeon0907@naver.com>
2025-03-08 00:37:30 +0900

    refactor: N + 1 문제 발생 가능성으로 Join Fetch 적용

--- src/main/java/com/zerobase/timate/service/CalendarService.java
+++ src/main/java/com/zerobase/timate/service/CalendarService.java
@@ -53,7 +53,7 @@ public class CalendarService {
 	public List<CalendarDto.Response> list(Long userId) {
 		// TODO: 캘린더 멤버인지 확인
 
-		List<UserCalendar> userCalendars = userCalendarRepository.findByUserId(userId);
+		List<UserCalendar> userCalendars = userCalendarRepository.findUserCalendarsWithCalendars(userId);
 
 		return userCalendars.stream()
 			.map(userCalendar -> CalendarDto.Response.from(userCalendar.getCalendar()))
