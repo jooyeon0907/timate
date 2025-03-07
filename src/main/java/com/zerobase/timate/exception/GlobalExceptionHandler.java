6703e273fde0812ba2c914f57534adccd9eec0cd
jooyeon <jooyeon0907@naver.com>
2025-03-08 00:36:02 +0900

    fix: 로그에서 예외 메시지와 스택 트레이스를 함께 출력하도록 수정

--- src/main/java/com/zerobase/timate/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zerobase/timate/exception/GlobalExceptionHandler.java
@@ -22,21 +22,21 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(AuthException.class)
 	public ResponseEntity<ApiResponse> handleAccountException(AuthException e){
-		log.error("{} is AuthException occurred.", e.getErrorMessage());
+		log.error("{} is AuthException occurred.", e.getErrorMessage(), e);
 
 		return ResponseEntity.badRequest().body(ApiResponse.error(e.getErrorCode()));
 	}
 
 	@ExceptionHandler(UserException.class)
 	public ResponseEntity<ApiResponse> handleUserException(UserException e){
-		log.error("{} is UserException occurred.", e.getErrorMessage());
+		log.error("{} is UserException occurred.", e.getErrorMessage(), e);
 
 		return ResponseEntity.badRequest().body(ApiResponse.error(e.getErrorCode()));
 	}
 
 	@ExceptionHandler(CalendarException.class)
 	public ResponseEntity<ApiResponse> handleCalendarException(CalendarException e){
-		log.error("{} is CalendarException occurred.", e.getErrorMessage());
+		log.error("{} is CalendarException occurred.", e.getErrorMessage(), e);
 
 		return ResponseEntity.badRequest().body(ApiResponse.error(e.getErrorCode()));
 	}
@@ -77,7 +77,7 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ApiResponse> handleException(Exception e){
-		log.error("{} is Exception occurred.", e.getMessage());
+		log.error("{} is Exception occurred.", e.getMessage(), e);
 
 		Map<String, String> errors = new HashMap<>();
     	errors.put("error", e.getMessage());
